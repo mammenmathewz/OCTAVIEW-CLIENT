8a6bf22d8512a433dd472bc38d406edbde3eb02d
Mammen Mathew <mammen999@gmail.com>
2025-01-01 23:07:28 +0530

    feat: update axios base URL to use environment variable and clean up App component

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function App() {
           </Route>
 
           <Route path="/meet" element={<Meet />} />
-          <Route path="/admin" element={<AdminDash />} />
+          <Route path="/admin" element={<AdminDash />} /> 
           <Route path="/admin-login" element={<AdminLogin />} />
         </Routes>
       </ErrorBoundary>
--- src/service/axios/axios.ts
+++ src/service/axios/axios.ts
@@ -2,8 +2,11 @@ import axios from "axios";
 import store from "../redux/store";
 import { selectAccessToken } from "../redux/store";
 
+console.log("url : ",import.meta.env.VITE_BASE_URL);
+
+
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api', 
+  baseURL: import.meta.env.VITE_BASE_URL, 
   headers: {
     'Content-Type': 'application/json',
   },
