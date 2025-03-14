fb823d7dcac82e24473465e73b223ee086e49dd4
Mammen Mathew <mammen999@gmail.com>
2025-03-15 03:28:32 +0530

    fix: update video source format in LandingBody component

--- src/components/user/home-login/LandingBody.tsx
+++ src/components/user/home-login/LandingBody.tsx
@@ -458,7 +458,7 @@ return (
         </div>
       </section>
       <VideoPlayerModal 
-    videoSrc={"https://octaview-data.s3.ap-south-1.amazonaws.com/images/invideo-ai-480+Revolutionize+Your+Hiring+with+Octaview!+2025-03-14.mp4"} // Update with your actual video source
+    videoSrc="https://octaview-data.s3.ap-south-1.amazonaws.com/images/invideo-ai-480+Revolutionize+Your+Hiring+with+Octaview!+2025-03-14.mp4"
     isOpen={isVideoModalOpen}
     onClose={() => setIsVideoModalOpen(false)}
   />
